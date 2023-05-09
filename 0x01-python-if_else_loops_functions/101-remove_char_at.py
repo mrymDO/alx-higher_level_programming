fb0b2154a8a5dd460e3895dfc06f61b5395b4527
Meryem <dobli.meryeme@gmail.com>
2023-05-09 15:15:27 -0700

    fix code, handle empty string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     str_c = str[:]
+    if not str_c:
+        return str_c
     while (n >= 0):
         for i in range(len(str_c)):
             if i == n:
