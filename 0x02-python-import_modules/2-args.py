1500becc9ba14020ce5c0a1b80450380dff4770a
Meryem <dobli.meryeme@gmail.com>
2023-05-11 08:03:20 -0700

    print list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:")
+    print("1:", str(sys.argv[1]))
+else:
+    print(len(sys.argv) - 1, "arguments:")
+    for i in range(1, len(sys.argv)):
+        print(f"{i}:", str(sys.argv[i]))
