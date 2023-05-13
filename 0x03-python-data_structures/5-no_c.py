cc5c81cd32bd19dc180d07005d8aa398931609c4
Meryem <dobli.meryeme@gmail.com>
2023-05-13 08:57:53 -0700

    fix pycodestyle for task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ''
     for c in my_string:
         if c != 'c' and c != 'C':
-            new_string += c 
+            new_string += c
     return new_string
