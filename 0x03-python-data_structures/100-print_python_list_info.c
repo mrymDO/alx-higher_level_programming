493f2c4e994d012de5f491085a3b07afa8d85a93
Meryem <dobli.meryeme@gmail.com>
2023-05-15 14:05:55 -0700

    fix2 Python/c API print basics info of a list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -21,7 +21,7 @@ void print_python_list_info(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		items = PyList_GetItem(p, i);
-		printf("Element %ld:\n%s\n", i, Py_TYPE(items)->tp_name);
+		printf("Element %ld:%s\n", i, Py_TYPE(items)->tp_name);
 		
 	}
 }
