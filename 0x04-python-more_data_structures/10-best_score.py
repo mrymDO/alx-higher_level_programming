6694b3c28831d99b23e5eb80e952add36a04b6fc
Meryem <dobli.meryeme@gmail.com>
2023-05-17 14:53:22 -0700

    fix3 the key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_val = max(a_dictionary)
-    return max_val
+    max_val = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == max_val:
+            return key
