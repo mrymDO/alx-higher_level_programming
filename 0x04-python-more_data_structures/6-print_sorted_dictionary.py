b2ad25f04d111b319a056d8f59528f5340f0b1a0
Meryem <dobli.meryeme@gmail.com>
2023-05-17 12:10:38 -0700

    fix3 print a dictionary bby ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dic = sorted(a_dictionary)
     for k in sorted_dic:
-        print("{} : {}".format(k, a_dictionary[k]))
+        print("{}: {}".format(k, a_dictionary[k]))
