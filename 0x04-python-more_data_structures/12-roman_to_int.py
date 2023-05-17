c7f7e83176f700ae51e4aa48679fac2e9f0cae00
Meryem <dobli.meryeme@gmail.com>
2023-05-17 16:17:01 -0700

    convert roman numerals

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    my_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string:
+        return 0
+    conv = 0
+    length = len(roman_string)
+    for i in range(length):
+        if i < length-1 and
+        my_dic[roman_string[i]] < my_dic[roman_string[i+1]]:
+            conv -= my_dic[roman_string[i]]
+        else:
+            conv += my_dic[roman_string[i]]
+    return conv
