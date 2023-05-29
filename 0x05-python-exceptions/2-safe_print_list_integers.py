acf0945d51b944779a8a279aef408b7fcb2ce27b
Meryem <dobli.meryeme@gmail.com>
2023-05-29 08:51:41 -0700

    fix2 print only integers in a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,6 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
+            if not str(my_list[i]).isdigit():
+                continue
             print("{:d}".format(my_list[i]), end="")
             count += 1
         print()
