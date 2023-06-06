1ea80380020b14c2dc20f519d5bee0e8ca37e178
Meryem <dobli.meryeme@gmail.com>
2023-06-06 06:22:31 -0700

    fix3 restrict creation of instance

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""module that prevent creating instance attributes"""
+
+
 class LockedClass:
+    """a class with no class or object attribute"""
     __slots__ = ['first_name']
 
     def __init__(self, first_name=''):
+        """initialize first_name"""
         self.first_name = first_name
