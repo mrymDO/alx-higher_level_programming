8b972508e6a0986622df7ecc48e758326d5e3010
Meryem <dobli.meryeme@gmail.com>
2023-06-12 06:53:22 -0700

    fix docstring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,11 +7,14 @@ Module: 5-base_geometry.py
 class BaseGeometry():
     def area(self):
         """returns error if area() is not implemented"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """validates value"""
+
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
