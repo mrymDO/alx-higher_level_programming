8ccad2fce835418d519b1d096241b9426c4fa516
Meryem <dobli.meryeme@gmail.com>
2023-06-12 05:40:16 -0700

    raise error if area is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """returns error if area() is not implemented"""
+    def area(self):
+        raise Exception("area() is not implemented")
