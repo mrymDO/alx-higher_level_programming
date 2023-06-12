50247ea21f413ee70bb4f36d777064870cdeba6d
Meryem <dobli.meryeme@gmail.com>
2023-06-12 06:09:41 -0700

    validate a value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    def area(self):
+        """returns error if area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
