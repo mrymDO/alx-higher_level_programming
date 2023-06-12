0b49b54b2951ecd4d2df857a4812dd99d71e2dad
Meryem <dobli.meryeme@gmail.com>
2023-06-12 04:57:37 -0700

    is a subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from
+Checks if an object is an instance of a class that inherited
+from a specified class; otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """return True or False"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
