34a7004066be37065c5dc21ddfd7dfe356156af3
Meryem <dobli.meryeme@gmail.com>
2023-06-12 10:45:27 -0700

    fix pycodestyle - square subclass of rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,7 +8,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """subclass of Rectangle"""
-    
+
     def __init__(self, size):
         """Instantiation with size"""
         self.integer_validator("size", size)
