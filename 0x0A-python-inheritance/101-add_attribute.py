18fa0b5cb70dd2897b9e5dbf2a194f332c7d9e3e
Meryem <dobli.meryeme@gmail.com>
2023-06-12 11:19:08 -0700

    checks if dict allows adding new attr base on the presence of __dict__

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,6 @@ Module: 101-add_attribute.py
 
 def add_attribute(obj, attr, value):
     """adds a new attribute to an object"""
-    if not hasattr(obj, attr):
-        setattr(obj, attr, value)
-    else:
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
