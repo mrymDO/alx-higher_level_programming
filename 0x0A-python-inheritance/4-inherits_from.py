07b6711edffa28aa94d864fc898d8e3b6dc0764a
Meryem <dobli.meryeme@gmail.com>
2023-06-12 05:09:34 -0700

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
         a_class: - class to evaluate
     """
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
