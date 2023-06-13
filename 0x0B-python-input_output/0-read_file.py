b009686f718b830e5641a8fd15c0c9b4563bd8e1
Meryem <dobli.meryeme@gmail.com>
2023-06-13 12:56:20 -0700

    fix docstring read a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+"""Module: 0-read_file that defines a function that reads a text file"""
+
+
 def read_file(filename=""):
     """a function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='UTF8') as a_file:
+
+    with open(filename, 'r', encoding='utf-8') as a_file:
         content = a_file.read()
-        print(content)
+        print(content, end="")
