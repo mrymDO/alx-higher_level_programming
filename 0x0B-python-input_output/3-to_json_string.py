847984329ff4d9278742b20b2ccfccc80b124832
Meryem <dobli.meryeme@gmail.com>
2023-06-13 13:21:35 -0700

    convert data to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module: 3-to_json_string that returns JSON representation of an object"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns JSON representation of a string"""
+
+    return json.dumps(my_obj)
