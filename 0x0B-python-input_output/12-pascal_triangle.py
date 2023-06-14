f3b683426f812f64add9c0ebac3bbcd4e063e840
Meryem <dobli.meryeme@gmail.com>
2023-06-14 12:06:29 -0700

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Module 12-pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing
+    Pascal's triangle of n rows
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+        row.append(1)
+        triangle.append(row)
+    return triangle
