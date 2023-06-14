0ba2bcca7f256ecaed5ce7477d67ff388cd11a3f
Meryem <dobli.meryeme@gmail.com>
2023-06-14 12:26:14 -0700

    search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Module 100-append_after """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ 
+    Inserts a line of a text to a file
+    after each line containing a specific string
+    """
+
+    with open(filename, 'r+', encoding='utf-8') as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            line = lines[i]
+
+            if search_string in line:
+                lines.insert(i + 1, new_string)
+        file.seek(0)
+
+        file.writelines(lines)
+
+        file.truncate()
