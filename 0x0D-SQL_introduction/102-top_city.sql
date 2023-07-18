8640cb480f650c6090690e670a812dbfc5cdf8b7
Meryem <dobli.meryeme@gmail.com>
2023-07-18 15:36:59 -0700

    fix topp 3

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Displays Top 3 of cities temperature
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month(date) IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
