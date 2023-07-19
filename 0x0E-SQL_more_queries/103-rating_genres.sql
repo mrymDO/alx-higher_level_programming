fb5bdd11e9f01e6a6e8f38fc15f4330389c45068
Meryem <dobli.meryeme@gmail.com>
2023-07-19 14:37:40 -0700

    fix typo error list all genres by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,4 +4,4 @@ FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
