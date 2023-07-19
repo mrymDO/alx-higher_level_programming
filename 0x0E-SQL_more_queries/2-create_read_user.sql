f1f72b55f4910c7cb423f7a4b5d42d8ebaccd544
Meryem <dobli.meryeme@gmail.com>
2023-07-19 09:48:42 -0700

    create user with read privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Create the database hbtn_0d_2 and the user user_0d_2
+
+-- Create Databse
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privileges on database
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
