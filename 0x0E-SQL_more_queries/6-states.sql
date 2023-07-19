a0c70a74a3bde062830b089397c581f1485dd2fc
Meryem <dobli.meryeme@gmail.com>
2023-07-19 10:34:51 -0700

    Create state table with id as primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Create Database and Table
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use database
+USE hbtn_0d_usa;
+
+-- Create Table
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
