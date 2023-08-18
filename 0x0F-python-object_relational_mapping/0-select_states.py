1e267caa650850208953306b84fa389d87bb2229
Meryem <dobli.meryeme@gmail.com>
2023-08-18 13:27:26 -0700

    add readme and 1st task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" a module taht lists all states from database """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+)
+cursor = connection.cursor()
+query = "SELECT * FROM states ORDER BY states.id"
+cursor.execute(query)
+states = cursor.fetchall()
+for state in states:
+    print(state)
+
+cursor.close()
+connection.close()
