43afbeab4169520ecd837d0b2881d2ef21c2c7ba
Meryem <dobli.meryeme@gmail.com>
2023-08-19 09:43:59 -0700

    print first state fix else statement

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
-        print("No states found")
+        print("nothing")
 
     session.close()
