289790835b1538a90f87295c5f19b987b33e8f02
Meryem <dobli.meryeme@gmail.com>
2023-08-31 09:20:27 -0700

    fix2 GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 #Get request
-response=$(curl -s -o /dev/stdout -w "%{http_code}" "$1")
-http_code="${response: -3}"
-if [ "$http_code" -eq 200 ];
-then
-    curl -s "$1"
-fi
+curl -sL '$1'
