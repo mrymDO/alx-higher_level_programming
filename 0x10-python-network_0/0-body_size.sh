5936ee20d5a019b99aea46cbb1a5baa2c904cd7d
Meryem <dobli.meryeme@gmail.com>
2023-08-31 08:01:10 -0700

    fix content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #  takes in a URL, sends a request to that URL
 # displays the size of the body of the response
-curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
+curl -sI "$1" | grep -iF 'Content-Length' | | awk '{print $2}'
