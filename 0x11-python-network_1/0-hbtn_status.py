9b4e68a5cb99db7815eecb7238f639c508a60951
Meryem <dobli.meryeme@gmail.com>
2023-09-01 07:01:00 -0700

    fetche url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Fetches an URL """
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
