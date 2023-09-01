8084854b1a3ee2edfe5e443bd6cd9dd191d1bc2e
Mrym <dobli.meryeme@gmail.com>
2023-09-01 08:47:24 -0700

    handling exception

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" displays the body response of an HTTP request handling exceptions """
+
+import sys
+from urllib import request
+from urllib import error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            response_data = response.read().decode('utf-8')
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
