0c81968ca2c8a20d104ddd338b6d3d35ad1d6613
Mrym <dobli.meryeme@gmail.com>
2023-09-01 10:55:29 -0700

    send an email address as a parameter to a URL and receive the response

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 
+send an email address as a parameter to a URL and receive the response
+HTTP POST request
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
