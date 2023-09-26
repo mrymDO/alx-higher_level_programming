9ac2d5638a915fa851fe0b5e6343f1a61bf22910
Meryeme <dobli.meryeme@gmail.com>
2023-09-26 12:12:50 -0700

    task7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const characterUrl of characters) {
+      request.get(characterUrl, (err, response, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
+    }
+  }
+});
